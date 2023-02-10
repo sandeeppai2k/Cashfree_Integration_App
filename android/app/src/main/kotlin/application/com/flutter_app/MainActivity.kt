4b21b33709d995326c4ce70f1ee8dd3b26071065
Sandeep Pai <sandeeppai@Sandeeps-MacBook-Air.local>
2023-02-10 20:54:50 +0530

    next commit

---
+++ android/app/src/main/kotlin/application/com/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package application.com.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/cashfree_test.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_cashfree_pg_sdk/api/cferrorresponse/cferrorresponse.dart';
+import 'package:flutter_cashfree_pg_sdk/api/cfpayment/cfdropcheckoutpayment.dart';
+import 'package:flutter_cashfree_pg_sdk/api/cfpaymentcomponents/cfpaymentcomponent.dart';
+import 'package:flutter_cashfree_pg_sdk/api/cfpaymentgateway/cfpaymentgatewayservice.dart';
+import 'package:flutter_cashfree_pg_sdk/api/cfsession/cfsession.dart';
+import 'package:flutter_cashfree_pg_sdk/api/cftheme/cftheme.dart';
+import 'package:flutter_cashfree_pg_sdk/utils/cfenums.dart';
+import 'package:flutter_cashfree_pg_sdk/utils/cfexceptions.dart';
+// import 'package:flutter_cashfree_pg_sdk_example/utils/util.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  var cfPaymentGatewayService = CFPaymentGatewayService();
+
+  @override
+  void initState() {
+    super.initState();
+    cfPaymentGatewayService.setCallback(verifyPayment, onError);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Cashfree Integration Test'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              TextButton(onPressed: pay, child: const Text("Confirm payment"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  void onError(CFErrorResponse errorResponse, String orderId) {
+    print('ERROR');
+  }
+
+  String orderId = "abc";
+  String paymentSessionId = "session_Vs5vBX2irjmmUxivgnyT5ib9t2kerDszZRCbJSOb8XaBsBEhl7PwKLK5MXMYK1vAhK8lA9GvXy-vZdhNYuA9L3bBPVpAsdKIYI9O17jxrqXp";
+  CFEnvironment environment = CFEnvironment.SANDBOX;
+
+  CFSession? createSession() {
+    try {
+      var session = CFSessionBuilder().setEnvironment(environment).setOrderId(orderId).setPaymentSessionId(paymentSessionId).build();
+      return session;
+    } on CFException catch (e) {
+      print(e.message);
+    }
+    return null;
+  }
+
+  pay() async {
+    try {
+      var session = createSession();
+      List<CFPaymentModes> components = <CFPaymentModes>[];
+      components.add(CFPaymentModes.UPI);
+      components.add(CFPaymentModes.CARD);
+      components.add(CFPaymentModes.WALLET);
+      var paymentComponent = CFPaymentComponentBuilder().setComponents(components).build();
+
+      var theme = CFThemeBuilder().setNavigationBarBackgroundColorColor("#FF0000").setPrimaryFont("Menlo").setSecondaryFont("Futura").build();
+
+      var cfDropCheckoutPayment = CFDropCheckoutPaymentBuilder().setSession(session!).setPaymentComponent(paymentComponent).setTheme(theme).build();
+
+      cfPaymentGatewayService.doPayment(cfDropCheckoutPayment);
+    } on CFException catch (e) {
+      print(e.message);
+    }
+
+  }
+
+}
